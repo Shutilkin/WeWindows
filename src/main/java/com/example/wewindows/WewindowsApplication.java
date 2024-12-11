259b9f10d9011547cf790a5d429bcd3aa8a57c45
Shutilkin <you@example.com>
2024-12-11 22:59:37 +0200

    Authe

---
+++ src/main/java/com/example/wewindows/WewindowsApplication.java
@@ -0,0 +1,13 @@
+package com.example.wewindows;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WewindowsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WewindowsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/wewindows/WewindowsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wewindows;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WewindowsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
