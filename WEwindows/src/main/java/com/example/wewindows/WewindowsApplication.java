d1a1539b7b6aaae4d5f1e8b40bb39b54dacde7a6
Shutilkin <you@example.com>
2024-12-11 23:52:02 +0200

    wewindows

--- WEwindows/src/main/java/com/example/wewindows/WewindowsApplication.java
+++ WEwindows/src/main/java/com/example/wewindows/WewindowsApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WewindowsApplication {
 
-	public static void main(String[] args) {
+	public static void main(String [] args) {
 		SpringApplication.run(WewindowsApplication.class, args);
 	}
 
